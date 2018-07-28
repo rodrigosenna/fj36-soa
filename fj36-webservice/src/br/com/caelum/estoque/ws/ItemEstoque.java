041983d5f53fa531cf1286def698655e60244be6
rodrigosenna <rodrigo.sonic@gmail.com>
2018-07-28 14:17:25 -0300

    Adicionando projeto fj36 soa

---
+++ fj36-webservice/src/br/com/caelum/estoque/ws/ItemEstoque.java
@@ -0,0 +1,34 @@
+package br.com.caelum.estoque.ws;
+
+public class ItemEstoque {
+	private String codigo;
+	private Integer quantidade;
+
+	// contrutor vazio only 
+	public ItemEstoque() {
+
+	}
+	
+	public ItemEstoque(String codigo, Integer quantidade) {
+		this.codigo = codigo;
+		this.quantidade = quantidade;
+	}
+	// GETTERS E SETTERS
+	
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+}
