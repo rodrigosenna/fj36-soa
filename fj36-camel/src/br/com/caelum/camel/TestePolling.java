267bd017d3ae30c0178a51c41f8b5e821bab3776
rodrigosenna <rodrigo.sonic@gmail.com>
2018-08-18 11:34:10 -0300

    7.13 EXERCÍCIO: REALIZANDO POLLING EM UM SERVIÇO

--- fj36-camel/src/br/com/caelum/camel/TestePolling.java
+++ fj36-camel/src/br/com/caelum/camel/TestePolling.java
@@ -1,9 +1,17 @@
 package br.com.caelum.camel;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import org.apache.camel.CamelContext;
-import org.apache.camel.LoggingLevel;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+
+import br.com.caelum.livraria.modelo.Livro;
 
 public class TestePolling {
 
@@ -11,13 +19,35 @@ public class TestePolling {
 
 		CamelContext context = new DefaultCamelContext();
 
+		/*
+		 * context.addRoutes(new RouteBuilder() {
+		 * 
+		 * @Override public void configure() throws Exception {
+		 * from("http://localhost:8088/fj36-livraria/loja/livros/mais-vendidos")
+		 * .delay(1000);
+		 * 
+		 * } });
+		 */
 		context.addRoutes(new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
-				from("http://localhost:8088/fj36-livraria/loja/livros/mais-vendidos")
-				.delay(1000);
+				from("http://localhost:8088/fj36-livraria/loja/livros/mais-vendidos").delay(1000).unmarshal().json()
+						.process(new Processor() {
+							@Override
+							public void process(Exchange exchange) {
+								List<?> listaDeLivros = (List<?>) exchange.getIn().getBody();
+								ArrayList<Livro> livros = (ArrayList<Livro>) listaDeLivros.get(0);
+								Message message = exchange.getIn();
+								message.setBody(livros);
+							}
+						});
+
 			}
 		});
+
+		context.start();
+		new Scanner(System.in).nextLine();
+		context.stop();
 
 	}
 
